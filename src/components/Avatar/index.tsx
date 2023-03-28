ab9638f1de017a2bd1d65245f353869e19e0ea68
André Weinfuter <aweinfurter@gmail.com>
2023-03-28 15:22:15 +0044

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="http://github.com/aweinfurter.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/aweinfurter.png" />
-    <Titulo fontSize={20}>André Weinfurter</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>André Weinfurter</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        aweinfurter
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Fullstack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 

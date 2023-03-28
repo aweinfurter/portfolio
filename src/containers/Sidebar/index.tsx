41e0d7aa9d288a126a723dcd60c7acedbdd4294d
André Weinfuter <aweinfurter@gmail.com>
2023-03-28 12:06:03 +0044

    Estrutura do projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/aweinfurter.png" />
+    <Titulo fontSize={20}>André Weinfurter</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+      quibusdam, voluptas quisquam temporibus quis incidunt possimus quaerat ad
+      nam illo est tempora, magni corrupti exercitationem laboriosam ducimus
+      enim voluptatum excepturi.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
